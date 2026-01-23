8edbf1cb32cf65d43d181dc533426350f747a499
hanveesh <179118013+Hanveesh@users.noreply.github.com>
2026-01-23 09:22:34 +0530

    Learned about  ConvexShape

--- src/main.cpp
+++ src/main.cpp
@@ -9,29 +9,19 @@ int main(){
     unsigned int height = 500;
     sf::RenderWindow window(sf::VideoMode({width,height}), "MyWindow");
 
-    window.setFramerateLimit(144);
-    float radius = 50.0;
-    sf::CircleShape circle(radius);
-    circle.setOrigin(circle.getGeometricCenter());
-    circle.setPosition({width / 4.0f , height/2.0f});
-    circle.setFillColor(sf::Color::Green);
-    circle.setOutlineThickness(6.5f);
-    circle.setOutlineColor(sf::Color::Red);
+    sf::ConvexShape convex;
+    convex.setPointCount(6);
+    
+    convex.setPoint(0, {0.f, -50.f});
+    convex.setPoint(1, {40.f, -20.f});
+    convex.setPoint(2, {25.f, 40.f});
+    convex.setPoint(3, {-25.f, 40.f});
+    convex.setPoint(4, {-40.f, -20.f});
+    convex.setPoint(5, {0.f, -50.f});
 
-
-    sf::RectangleShape rectangle({30.0f,70.0f});
-    rectangle.setOrigin(rectangle.getSize() / 2.0f);
-    rectangle.setPosition({width / 4.0f , height/2.0f});
-    rectangle.setFillColor(sf::Color::Red);
-    rectangle.setOutlineThickness(5.0f);
-    rectangle.setOutlineColor(sf::Color::Green);
-
-
-
-    int r = 0;
-    int s = 2;
-
-    circle.setPointCount(3);
+    convex.setOrigin(convex.getGeometricCenter());
+    convex.setFillColor(sf::Color::Green);
+    convex.setPosition({width/2.0f,height/2.0f});
     while(window.isOpen()){
 
         while (const std::optional event = window.pollEvent())
@@ -49,47 +39,10 @@ int main(){
         }
 
 
-
-        std::cout<<r<<std::endl;
-
-        if(r == 0){
-            std::cout<<circle.getPosition().x;
-            if(circle.getPosition().x >= (width - radius)){
-                s+=2;
-                r = 1;
-            }
-            else{
-                std::cout<<" moving Right";
-                circle.rotate(sf::degrees(s));
-                circle.move({1,0});
-            }
-        }
-
-        if (r == 1){
-            if(circle.getPosition().x <= (radius)){
-                s+=2;
-                r = 0;
-            }
-            else{
-                std::cout<<" moving Left";
-                circle.rotate(sf::degrees(-s));
-                circle.move({-1,0});
-            }            
-        }
-
-        circle.setFillColor(sf::Color::Green);
-        rectangle.setFillColor(sf::Color::Red);
-
-        if(circle.getGlobalBounds().findIntersection(rectangle.getGlobalBounds())){
-            std::cout<<"Intersected";
-            circle.setFillColor(sf::Color::White);
-            rectangle.setFillColor(sf::Color::White);
-        }
-
-
         window.clear();   
 
-        window.draw(circle);window.draw(rectangle);
+        window.draw(convex);
+
         window.display();
 
 
